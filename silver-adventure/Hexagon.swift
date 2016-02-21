99a921c212661badc3049dba188207ca6fd97b6e
Nick Belzer <nickbelzer@gmail.com>
2016-02-21 21:27:08 +0100

    Added in Tiles, Water, Land, Mountain

--- silver-adventure/Hexagon.swift
+++ silver-adventure/Hexagon.swift
@@ -11,10 +11,17 @@ protocol Hexagon {
   var coordinate: Axialcoordinate { get set }
 }
 
+enum TileType {
+  case Water
+  case Land
+  case Mountain
+}
+
 class Tile: Hexagon {
   
   let sprite: SKSpriteNode;
   var coordinate: Axialcoordinate;
+  let tileType: TileType;
   
   var height: Float;
   
@@ -25,8 +32,24 @@ class Tile: Hexagon {
     sprite.xScale = 0.2;
     sprite.yScale = sprite.xScale;
     sprite.colorBlendFactor = 1.0;
-    sprite.color = UIColor(red: CGFloat(height), green: 0.4, blue: 0.5, alpha: 1)
+//    sprite.color = UIColor(red: CGFloat(height), green: 0.4, blue: 0.5, alpha: 1)
     
     self.height = height;
+
+    if (height <= 0.4) { tileType = TileType.Water }
+    else if (height <= 0.7) { tileType = TileType.Land }
+    else { tileType = TileType.Mountain }
+    
+    switch (tileType) {
+    case TileType.Water:
+      sprite.color = UIColor(hue: 205/360.0, saturation: 0.8, brightness: CGFloat(height/0.8) + 0.35, alpha: 1.0)
+      break
+    case TileType.Land:
+      sprite.color = UIColor(hue: 85/360.0, saturation: 1.0, brightness: CGFloat((height-0.4)/0.7) + 0.5, alpha: 1.0)
+      break
+    case TileType.Mountain:
+      sprite.color = UIColor(hue: 0/360.0, saturation: 0, brightness: CGFloat((height-0.7)/0.9) + 0.2, alpha: 1.0)
+      break
+    }
   }
 }
