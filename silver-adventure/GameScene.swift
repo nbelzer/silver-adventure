d1b4cb1755794074c1b7dd73e7249426a4544231
Nick Belzer <nickbelzer@gmail.com>
2016-02-25 13:16:35 +0100

    Made moving the island possible

--- silver-adventure/GameScene.swift
+++ silver-adventure/GameScene.swift
@@ -21,11 +21,28 @@ class GameScene: SKScene {
     
     grid = HexGrid(toNode: centerNode)
     grid.createGrid(10);
+    
+//    centerNode.runAction(SKAction.scaleBy(2, duration: 20));
   }
 
   override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
     /* Called when a touch begins */
-    grid.createGrid(10)
+//    grid.createGrid(10)
+  }
+  
+  override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+    for touch in touches {
+      let curLoc = touch.locationInView(self.view);
+      let prevLoc = touch.previousLocationInView(self.view);
+      
+      let deltaX = curLoc.x - prevLoc.x;
+      let deltaY = curLoc.y - prevLoc.y;
+      
+      if (abs(deltaX) > 3 || abs(deltaY) > 3) {
+        grid.node.position.x += deltaX/2;
+        grid.node.position.y -= deltaY/2;
+      }
+    }
   }
 
   override func update(currentTime: CFTimeInterval) {
