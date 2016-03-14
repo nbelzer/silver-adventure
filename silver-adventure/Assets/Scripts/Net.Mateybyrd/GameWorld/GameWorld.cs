df4aa57bc8d1fc327084da8871ae43a83e2c0817
Nick Belzer <nickbelzer@gmail.com>
2016-03-14 22:12:24 +0100

    Implemented basic grid test

--- silver-adventure/Assets/Scripts/Net.Mateybyrd/GameWorld/GameWorld.cs
+++ silver-adventure/Assets/Scripts/Net.Mateybyrd/GameWorld/GameWorld.cs
@@ -1,8 +1,14 @@
 using System;
+using Net.Mateybyrd.GameWorld.Grid;
 
 namespace Net.Mateybyrd.GameWorld {
   
-  public class GameWorld {
+  public class GameWorld<T> {
+   
+    public GridManager<T> Grid;
     
+    public GameWorld() {
+      Grid = new GridManager<T>();
+    }
   }
 }
--- silver-adventure/Assets/Scripts/net.mateybyrd/TestScript.cs
+++ silver-adventure/Assets/Scripts/net.mateybyrd/TestScript.cs
@@ -1,18 +1,45 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
+using Net.Mateybyrd.GameWorld;
+using Net.Mateybyrd.GameWorld.Grid;
 
 namespace Net.Mateybyrd {
   
   public class TestScript : MonoBehaviour {
 
+    public GameWorld<CubePosition> World;
+
     // Use this for initialization
     void Start () {
-      Debug.Log ("Hello world");
+      World = new GameWorld<CubePosition>();
+      CreateSampleWorld(5);
+
+      foreach (var item in World.Grid.GetTileAt(new CubePosition(1, 1, -2)).GridPosition.GetNeighbours()) {
+        Debug.Log(item.XPos + ", " + item.YPos + ", " + item.ZPos);
+      }
     }
 
     // Update is called once per frame
     void Update () { 
         
+    }
+    
+    void OnDrawGizmos() {
+      foreach (KeyValuePair<CubePosition, Tile<CubePosition>> pair in World.Grid.GetGrid()) {
+        Gizmos.DrawCube(pair.Key.GetWorldPosition(), Vector3.one * 0.5f);
+      }
+    }
+
+    public void CreateSampleWorld(int gridSize) {
+      for (var x = -gridSize; x <= gridSize; x++) {
+        for (var z = -gridSize; z <= gridSize; z++) {
+          var y = -x-z;
+
+          if (y >= -gridSize && y <= gridSize) {
+            World.Grid.AddTile(new Tile<CubePosition>(new CubePosition(x, y ,z), Vector3.zero));
+          }
+        }
+      }
     }
   }
 }
